1228de5c393918f2b3cae25ca0443450266b7613
Damm Kewl <25-dammkewl@users.noreply.code.samourai.io>
2024-02-01 22:48:30 +0100

    Test branch

--- overlays/RoninOS/usr/local/sbin/ronin-setup.sh
+++ overlays/RoninOS/usr/local/sbin/ronin-setup.sh
@@ -36,7 +36,7 @@ cd "$HOME" || exit
 sleep 75s 
 
 # Clone Repo
-git clone -b master https://code.samourai.io/ronindojo/RoninDojo /home/ronindojo/RoninDojo
+git clone -b feature/backup_includes_more https://code.samourai.io/ronindojo/RoninDojo /home/ronindojo/RoninDojo
 cd /home/ronindojo/RoninDojo
 
 # Source files
